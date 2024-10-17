5dd9c191f33bdff00b0960b8390d6297d500d43b
kuchobang <obangoriemi@gmail>
2024-10-17 23:16:53 +0300

    creating user model

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 
 mongoose.connect(process.env.MONGODB_URL)
 .then(() =>{
-    console.log("mongodb connected successfully")
+    console.log("mongodb connected")
 })
 .catch((error) =>{
    console.log(error)

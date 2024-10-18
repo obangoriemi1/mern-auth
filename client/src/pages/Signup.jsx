13042a2bd6d69c94e68a8e0ceafaa765f1746f61
kuchobang <obangoriemi@gmail>
2024-10-18 19:21:00 +0300

    complete sign up page functionality

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,16 +1,56 @@
-import React from 'react'
+import  { useState } from 'react'
 import { Link } from 'react-router-dom'
+
+
 const Signup = () => {
+const [formData, setFormData] = useState({})
+const [error, setError] = useState(false)
+const [loading, setLoading] = useState(false)
+  const handleChange = (e) =>{
+    setFormData({...formData, [e.target.id]: e.target.value})
+  }
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault()
+    try {
+      setLoading(true)
+      setError(false)
+      const response = await fetch ("/api/auth/signup",{
+        method: "POST",
+        headers:{
+          "content-Type": "application/json",
+        },
+        body: JSON.stringify(formData)
+       
+      })
+      const data = await response.json()
+      setLoading(false);
+      if(data.success === false){
+        setError(true)
+        return;
+      }
+      
+    } catch (error) {
+      setLoading(false)
+      setError(true)
+    }
+   
+   
+
+  }
+ 
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>
         Sign Up
         </h1>
-        <form className='flex flex-col gap-4'>
-          <input type="text" placeholder='Enter name' id='name' className='bg-slate-100 p-3 rounded-lg'/>
-          <input type="email" placeholder='Enter email' id='email' className='bg-slate-100 p-3 rounded-lg'/>
-          <input type="password" placeholder='Enter password' id='password' className='bg-slate-100 p-3 rounded-lg'/>
-          <button className='bg-slate-800 text-white p-3 rounded-lg uppercase hover:opacity-95'>Sign Up</button>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+          <input onChange={handleChange} type="text" placeholder='Enter name' id='name' className='bg-slate-100 p-3 rounded-lg'/>
+          <input onChange={handleChange}  type="email" placeholder='Enter email' id='email' className='bg-slate-100 p-3 rounded-lg'/>
+          <input onChange={handleChange}  type="password" placeholder='Enter password' id='password' className='bg-slate-100 p-3 rounded-lg'/>
+          <button disabled={loading} className='bg-slate-800 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+            {loading ? "loading": "Sign up."}
+            </button>
 
         </form>
         <div className="flex gap-4  mt-5">
@@ -21,7 +61,7 @@ const Signup = () => {
          <span  className='text-blue-500'>Sign in</span>
          </Link>
         </div>
-
+         <p className='text-red-700 mt-5'>{error && "some thing went wrong"}</p>
 
     </div>
   )
--- index.js
+++ index.js
@@ -3,17 +3,19 @@ import express from "express"
 import dotenv from "dotenv"
 import userRoute from "./backend/routes/userRoute.js" 
 import authRoute from "./backend/routes/authRoute.js" 
+//import cors from "cors"
 dotenv.config()
 
 const app = express()
 app.use(express.json())
+//app.use(cors())
 mongoose.connect(process.env.MONGODB_URL)
 .then(() =>{
     console.log("mongodb connected")
 })
 .catch((error) =>{
    console.log(error)
-})
+})  
 
 app.listen(3000, () =>{
     console.log("sever listening on port 3000!")
